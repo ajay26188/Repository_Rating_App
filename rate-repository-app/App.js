0a43ab893caa941b449685817ab02cef19386a98
ajay26188 <ajaysah52@gmail.com>
2025-07-05 14:31:30 +0530

    rate-repository-api added

--- rate-repository-app/App.js
+++ rate-repository-app/App.js
@@ -1,13 +1,20 @@
 import Main from './src/components/Main';
 import {SafeAreaProvider} from 'react-native-safe-area-context'
 import {NativeRouter} from 'react-router-native';
+import {ApolloProvider} from '@apollo/client';
 
+import createApolloClient from './src/utils/apolloClient';
+
+const apolloClient = createApolloClient();
 
 const App = () => {
+
   return (
     <NativeRouter>
       <SafeAreaProvider>
+      <ApolloProvider client={apolloClient}>
         <Main />
+      </ApolloProvider>  
       </SafeAreaProvider>
     </NativeRouter>
 
